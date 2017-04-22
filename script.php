636feb12ba34ee22428865a338541e55ee4824fb
Dahmer <pauldahmer294@gmail.com>
2017-04-22 12:23:01 +0000

    needed

--- script.php
+++ script.php
@@ -3,6 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>assistant</title>
+    <style media="screen">
+      body{
+        background-color: rgb(0, 0, 0);
+        color: white;
+      }
+      div{
+        border: 30px;
+        background-color: rgba(255, 255, 255, 0.53)
+      }
+    </style>
   </head>
   <body>
     <h1>you can ask me anything</h1>
